1f2acde2cf0d5cf4c5dae8bea3cc037a1129a70b
JUDY <s2018s05@e-mirim.hs.kr>
2020-06-05 09:04:46 +0900

    초기 커밋.

---
+++ overridemethods/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overridemethods
+{
+    class Program
+    {
+        static int Power(int input)
+        {
+            return input*input;
+        }
+        static int Power(int input, int count)
+        {
+            return (int)Math.Pow(input, count);
+        }
+        static int SumAll(int end)
+        {
+            int total = 0;
+
+            for (int i = 0; i < end; i++)
+            {
+                total += i;
+            }
+            return total;
+        }
+        static int SumAll(int start, int end)
+        {
+            int total = 0;
+            for (int i = start; i < end; i++)
+            {
+                total += i;
+            }
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Power(2));
+            Console.WriteLine(Power(2, 3));
+            Console.WriteLine(SumAll(3));
+            Console.WriteLine(SumAll(1, 4));
+
+        }
+    }
+}
